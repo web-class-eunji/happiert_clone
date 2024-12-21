6750497617583eeb9af22b4bcfadb60e5fe99aa2
Mong <moeunji12@gmail.com>
2024-12-21 15:53:07 +0900

    day7숙제 day8에 검사
    day7숙제 day8에 검사

--- script/script.js
+++ script/script.js
@@ -1,13 +1,27 @@
-window.addEventListener("scroll",() => {
-    const scrollBar = document.querySelector(".scroll_bar");
-    const scrollY = window.scrollY;
-    const scrollHeight = document.body.scrollHeight - window.innerHeight;
-    // document.body.scrollHeight : 문서의 전체 높이
-    // window.innerHeight : 화면에 보여지는 높이
-    // 두 값을 빼면 스크롤 할 수 있는 거리를 구할 수 있다. 
+const header = document.querySelector("header");
+// 헤더 요소 가져오기
 
-    const scrollPercent = (scrollY / scrollHeight) * 100;
-    // (현재 스크롤 위치 / 스크롤 가능한 전체 길이) * 100
+let posY = 0;
+// 스크롤 위치값 넣을 변수 생성
 
-    scrollBar.style.width = `${scrollPercent}%`
+window.addEventListener("scroll", (e) => {
+    // 스크롤 했을 때
+    if (posY < window.scrollY) {
+        //posY(0)이 윈도우의 스크롤 값보다 작다면
+        // 스크롤 하면 window.scrollY의 값이 생기니까 0보다 커짐!
+        headerHide();
+        // 헤더를 숨기는 함수를 실행
+    } else {
+        headerShow();
+    }
+    posY = window.scrollY;
+    // 스크롤을 내렸다면 새로운 값이 들어있을것!
 });
+
+function headerHide() {
+    header.classList.add("hide");
+}
+
+function headerShow() {
+    header.classList.remove("hide");
+}
